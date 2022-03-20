563c1aff41df5e0d3a4049d9d9ff6637bb21aa88
Rudney Vinicius <rudney_vinicius@hotmail.com>
2022-03-20 18:34:57 -0300

    alteração_2
    uso do for e Encerramento do programa

--- Pacote_Download/Estudos.py
+++ Pacote_Download/Estudos.py
@@ -4,13 +4,16 @@ print('_____________________________________________')
 
 numero_secreto = 42
 totativa = 3
-rodada = 1
 
-while(rodada <= totativa):
-    print('Tentativas', rodada,'de', totativa)
-    chute_str = input('Digite seu numero ')
+for rodada in range(1, totativa +1):
+    print('Tentativa {} de {}'.format(rodada, totativa))
+    chute_str = input('Digite seu número ')
     print('Você digitou', chute_str)
     chute = int(chute_str)
+
+    if(chute <1 or chute > 100):
+        print('Você deve digitar um número entre 1 e 100!')
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute  > numero_secreto   
@@ -18,11 +21,11 @@ while(rodada <= totativa):
 
     if(acertou):
         print('Você acertou')
+        break
     else:
         if(maior):
             print('Você errou! O seu chute foi maior ')
         elif(menor):
             print('Você errou! O seu chute foi menor ')
 
-    rodada = rodada + 1
     print('Fim do jogo')
