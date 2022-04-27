9c902f4893478bf4bfe88eef760d2165ef2c793f
Rudney Vinicius <rudney_vinicius@hotmail.com>
2022-04-27 14:06:53 -0300

    Complemento forca.py

--- Pacote_Download/forca.py
+++ Pacote_Download/forca.py
@@ -8,5 +8,24 @@ def jogar():
     print('_____________________________________________')
     print('________Bem vindo ao jogo de forca___________')
     print('_____________________________________________')  
+    
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input('Qual letra? ')
+
+        for  letra in palavra_secreta:
+            if(chute == letra):
+                print(chute)
+
+        print('jogando ...')
 
     print('Fim do jogo')
+
+if(__name__ == "__main__"):
+    jogar()
+ 
