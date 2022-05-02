828ca51c24de6d6a8ead95bd8d40821a9be416a3
Rudney Vinicius <rudney_vinicius@hotmail.com>
2022-05-02 17:03:05 -0300

    Limitando o laço

--- Pacote_Download/forca.py
+++ Pacote_Download/forca.py
@@ -8,13 +8,13 @@ def jogar():
     print('________Bem vindo ao jogo de forca___________')
     print('_____________________________________________')  
     
-    palavra_secreta = 'batata'
+    palavra_secreta = 'mel'
     print('A palavra secreta tem',len (palavra_secreta), 'letras')
     letra_acertadas = ['_', '_', '_', '_', '_', '_']
 
     enforcou = False
     acertou = False
-
+    erros = 0
     
     print(letra_acertadas)
 
@@ -23,12 +23,16 @@ def jogar():
         chute = input('Qual letra? ')
         chute = chute.strip()
 
-        index = 0
-        for digito in palavra_secreta:
-            if(chute.upper() == digito.upper()):
-                letra_acertadas[index] = digito
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for digito in palavra_secreta:
+                if(chute.upper() == digito.upper()):
+                    letra_acertadas[index] = digito
+                index = index + 1
+        else:
+            erros = erros + 1
 
+        enforcou = erros == len(palavra_secreta)
         print(letra_acertadas)
 
     print('Fim do jogo')
