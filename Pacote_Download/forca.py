b33989012677573fe4e4be95d22854205f76a373
Rudney Vinicius Tozim messias <rudney_vinicius@hotmail.com>
2022-05-10 12:27:50 -0300

    Definindo Função final

--- Pacote_Download/forca.py
+++ Pacote_Download/forca.py
@@ -2,29 +2,14 @@
 #def nome_da_funcao():
     # todo o código identado faz parte da função print("aprendendo funções")#
 
+
 import random
 
 def jogar():
-
-    print('_____________________________________________')
-    print('________Bem Vindo ao jogo de forca___________')
-    print('_____________________________________________')  
     
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    print('A palavra secreta tem',len (palavra_secreta), 'letras')
-
-    letra_acertadas = ["_" for letra in palavra_secreta]
+    imprimir_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letra_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -55,6 +40,29 @@ def jogar():
     else:
         print('Você perdeu!! a palavra secreta é {}'.format(palavra_secreta))
     print('Fim do jogo')
+
+def imprimir_mensagem_abertura():
+        print('_____________________________________________')
+        print('________Bem Vindo ao jogo de forca___________')
+        print('_____________________________________________') 
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    print('A palavra secreta tem',len (palavra_secreta), 'letras')
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 if(__name__ == "__main__"):
         jogar() 
